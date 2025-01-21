50e0419431c4fa360b17dd77e706f40350e81717
sriAtavi <sridargwoda13@gmail.com>
2025-01-21 14:31:30 +0530

    project initiated

---
+++ src/main/java/com/Atavi/bsm/BloodSampleManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Atavi.bsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodSampleManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodSampleManagementSystemApplication.class, args);
+	}
+
+}
